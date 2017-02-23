fe843bd54b278e561d247e3bc6895624f0c0481d
Nicholas Roubal <nroubal@usc.edu>
2017-02-23 12:53:44 -0800

    WIP

--- sketch.js
+++ sketch.js
@@ -1,12 +1,40 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  fill(255);
+
   if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+    fill(255,0,0);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+
+  stroke(0);
+  ellipse(mouseX, mouseY, 60, 60);
+
+  // foreground
+  fill(255);
+  stroke(0);
+  rect(width * 0.25, height * 0.25, 0.5 * width, 0.5 * height);
+
+  if(mouseX > (width * 0.25 + 30) && mouseX < (width * 0.75 - 30) && mouseY > (height * 0.25 + 30) && mouseY < (height * 0.75 - 30))
+  {
+    fill(255,0,0);
+    stroke(255,0,0);
+    fill(0);
+    stroke(0);
+
+    ellipse(mouseX, mouseY, 60, 60);
+  }
+  
+  //fill(0);
+  stroke(255);
+  textAlign(CENTER);
+  textSize(width * 0.03);
+  text("nicholasroubal@gmail.com\nwebsite wip :-)", 0.5 * width , 0.5 * height);
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
